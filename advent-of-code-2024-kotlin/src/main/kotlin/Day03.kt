78e719536f1d48bf690accf32ede33aae920c15a
Fer de Sande <fernando.desande@bitingbit.com>
2024-12-10 18:21:29 +0000

    Solve day 03

---
+++ advent-of-code-2024-kotlin/src/main/kotlin/Day03.kt
@@ -0,0 +1,39 @@
+package org.fdesande
+
+class Day03 : AdventProblem {
+
+    companion object {
+        private const val FIRST_INPUT = "/input03-1.txt"
+        private const val SECOND_INPUT = "/input03-2.txt"
+    }
+
+    override fun firstPart(): String {
+        // Valid result: 159833790
+        return extractMultipliers(
+            readFile(FIRST_INPUT)
+        ).sumOf { multipliers -> multipliers[0] * multipliers[1] }.toString()
+    }
+
+    override fun secondPart(): String {
+        // Valid result: 89349241
+        return readFile(SECOND_INPUT)
+            .joinToString("\\n")
+            .split(Regex("don't\\(\\).*?do\\(\\)")) // Hint: .*? is a no greedy .*
+            .let { lines -> extractMultipliers(lines) }
+            .sumOf { multipliers -> multipliers[0] * multipliers[1] }.toString()
+    }
+
+    private fun readFile(fileName: String): List<String> {
+        return (javaClass.getResource(fileName)?.readText()
+            ?: throw IllegalStateException("File not found: $fileName"))
+            .split("\n")
+            .filter { line -> line.isNotBlank() }
+    }
+
+    private fun extractMultipliers(lines: List<String>): List<List<Int>> {
+        return lines
+            .map { line -> Regex("mul\\(\\d+,\\d+\\)").findAll(line).map { it.value }.toList() }
+            .flatten()
+            .map { op -> Regex("\\d+").findAll(op).map { it.value.toInt() }.toList() }
+    }
+}
