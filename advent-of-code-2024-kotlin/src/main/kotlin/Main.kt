3c3922e49c50a958d1db9c5f57bc41721db8302d
Fer de Sande <fernando.desande@bitingbit.com>
2024-12-10 16:48:28 +0000

    Create project

---
+++ advent-of-code-2024-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package org.fdesande
+
+fun main() {
+    val problem = Day01()
+
+    println("The result of the first part is: ${problem.firstPart()}")
+    println("The result of the second part is: ${problem.secondPart()}")
+}
