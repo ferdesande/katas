e2c0c31469326122c4d7048f8cc7af7b93a447c8
Fer de Sande <fernando.desande@bitingbit.com>
2024-12-18 14:49:56 +0000

    Solve day 11

---
+++ advent-of-code-2024-kotlin/src/main/kotlin/Day11.kt
@@ -0,0 +1,47 @@
+package org.fdesande
+
+import org.fdesande.common.FileUtils
+
+class Day11 : AdventProblem {
+
+    companion object {
+        private const val INPUT = "/input11.txt"
+    }
+
+    // Valid result: 185894
+    override fun firstPart(): String = calculateStonesEvolution(25).toString()
+
+    // Valid result: 221632504974231
+    override fun secondPart(): String = calculateStonesEvolution(75).toString()
+
+    private fun calculateStonesEvolution(blinkCount: Int): Long {
+        var stones = FileUtils.getLines(INPUT).first()
+            .split(" ")
+            .map { it.toLong() }.groupingBy { it }.eachCount()
+            .mapValues { it.value.toLong() }
+
+        repeat(blinkCount) { stones = blink(stones) }
+        return stones.entries.sumOf { it.value }
+    }
+
+    private fun blink(stones: Map<Long, Long>): Map<Long, Long> =
+        stones.entries.map { entry -> transform(entry.key).map { it to entry.value } }
+            .flatten()
+            .fold(mutableMapOf()) { map, entry ->
+                map[entry.first] = entry.second + (map[entry.first] ?: 0)
+                map
+            }
+
+    private fun transform(stone: Long): List<Long> {
+        val value = stone.toString()
+        return when {
+            stone == 0L -> listOf(1L)
+            value.length % 2 == 0 -> {
+                val half = value.length / 2
+                listOf(value.substring(0 until half), value.substring(half)).map { it.toLong() }
+            }
+
+            else -> listOf(stone * 2024)
+        }
+    }
+}
