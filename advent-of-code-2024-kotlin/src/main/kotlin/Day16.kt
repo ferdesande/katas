1c322a670e8936807b28f6f1c70392a60eb5a376
Fer de Sande <fernando.desande@bitingbit.com>
2024-12-21 19:59:30 +0000

    Solve day 16

--- advent-of-code-2024-kotlin/src/main/kotlin/Day16.kt
+++ advent-of-code-2024-kotlin/src/main/kotlin/Day16.kt
@@ -1,72 +1,52 @@
 package org.fdesande
 
-import org.fdesande.common.Direction
-import org.fdesande.common.FileUtils
-import org.fdesande.common.Grid
-import org.fdesande.common.Point
+import org.fdesande.common.*
 
 class Day16 : AdventProblem {
 
     companion object {
         private const val INPUT = "/input16.txt"
-        private const val SAMPLE = "/input16-sample.txt"
 
         private const val START = 'S'
         private const val END = 'E'
         private const val WALL = '#'
 
         private const val STRAIGHT_SCORE = 1
-        private const val TURN_SCORE = 1001
+        private const val TURN_SCORE = 1000
+    }
+
+    private var bestPath: Path?
+
+    init {
+        val grid = FileUtils.getGrid(INPUT)
+        bestPath = getBestPaths(grid)
     }
 
     override fun firstPart(): String {
         // Valid result: 106512
-        val grid = FileUtils.getGrid(INPUT)
-        return getBestPaths(grid).minOfOrNull { it.last().score }.toString()
+        return (bestPath?.score ?: Int.MAX_VALUE).toString()
     }
 
     override fun secondPart(): String {
-        // Valid result:
-        val grid = FileUtils.getGrid(SAMPLE)
-        TODO()
+        // Valid result: 563
+        return bestPath?.visited?.size?.toString() ?: "Error"
     }
 
-    private fun getBestPaths(grid: Grid): List<Set<MazePoint>> {
-        val visited = mutableMapOf<Point, Int>()
-        val start = grid.getSingleCharacterPoint(START)
-        var paths = setOf(setOf(MazePoint(start, Direction.RIGHT, 0)))
+    private fun getBestPaths(grid: Grid): Path? {
+        val start = Path(grid.getSingleCharacterPoint(START))
+        val watcher = PathWatcher(grid, start.current)
+
+        var paths = setOf(start)
         var bestScore = Int.MAX_VALUE
-        var bestPaths: MutableList<Set<MazePoint>> = mutableListOf()
+        var bestPaths: MutableList<Path> = mutableListOf()
 
         while (paths.isNotEmpty()) {
-            val pathsWithScoreByPoint = paths
-                .filterNot { path -> grid.getValue(path.last().point) == END }
-                .flatMap { path ->
-                    path.last().getNeighbors()
-                        .asSequence()
-                        .filter { grid.isInGrid(it.point) }
-                        .filterNot { grid.getValue(it.point) == WALL }
-                        .filter { (visited[it.point] ?: Int.MAX_VALUE) > it.score }
-                        .filterNot { bestScore <= it.score }
-                        .map { path + it }
-                        .toList()
-                }
-                .groupBy({ it.last().point }, { Pair(it, it.last().score) })
-            val minScoreByPoint = pathsWithScoreByPoint.mapValues { pairs -> pairs.value.minOf { it.second } }
-            paths = pathsWithScoreByPoint.entries
-                .flatMap { entry ->
-                    entry.value
-                        .filter { it.second == minScoreByPoint[entry.key] }
-                        .map { pair -> pair.first }
-                }
-                .toSet()
+            paths = watcher.watch(paths.flatMap { it.move() })
 
-            val justVisited = paths.map { it.last() }
-            visited += justVisited.map { it.point to it.score }
             paths
-                .filter { grid.getValue(it.last().point) == END }
+                .filter { watcher.isEnd(it) }
                 .forEach { path ->
-                    val score = path.last().score
+                    val score = path.score
                     if (score == bestScore) {
                         bestPaths += path
                     } else if (score < bestScore) {
@@ -75,36 +55,99 @@ class Day16 : AdventProblem {
                     }
                 }
         }
-        return bestPaths
+        return watcher.merge(bestPaths)
     }
 
-    private data class MazePoint(val point: Point, val direction: Direction, val score: Int) {
-        fun getNeighbors(): List<MazePoint> {
-            return when (direction) {
+    private class PathWatcher(private val grid: Grid, start: DirectedPoint) {
+        private enum class BiDirection { VERTICAL, HORIZONTAL }
+
+        private fun Direction.toBidirectional() = when (this) {
+            Direction.UP, Direction.DOWN -> BiDirection.VERTICAL
+            Direction.RIGHT, Direction.LEFT -> BiDirection.HORIZONTAL
+        }
+
+        private val visited = mutableMapOf(
+            Pair(start.point, start.direction.toBidirectional()) to 0
+        )
+
+        fun watch(paths: List<Path>): Set<Path> {
+            val alivePaths = paths
+                .filter { isInGrid(it) }
+                .filterNot { isInWall(it) }
+                .filter { shouldVisit(it) }
+
+            updateVisited(alivePaths)
+            return mergeByPoint(alivePaths).toSet()
+        }
+
+        fun isEnd(path: Path): Boolean = grid.getValue(path.current.point) == END
+        fun merge(paths: List<Path>): Path? =
+            when (paths.size) {
+                0 -> null
+                1 -> paths.single()
+                else -> {
+                    val minScore = paths.minOfOrNull { it.score }
+                    val joinedPoints = paths.filter { it.score == minScore }.flatMap { it.visited }.toSet()
+                    val first = paths.first()
+                    Path(first.current, joinedPoints, first.score)
+                }
+            }
+
+        private fun isInGrid(path: Path): Boolean = grid.isInGrid(path.current.point)
+        private fun isInWall(path: Path): Boolean = grid.getValue(path.current.point) == WALL
+        private fun shouldVisit(path: Path): Boolean {
+            val currentScore = visited[Pair(path.current.point, path.current.direction.toBidirectional())]
+            return currentScore == null || currentScore > path.score
+        }
+
+        private fun mergeByPoint(paths: List<Path>): List<Path> =
+            paths.groupBy({ it.current }, { it })
+                .mapNotNull { entry -> merge(entry.value) }
+
+        private fun updateVisited(alivePaths: List<Path>) {
+            alivePaths
+                .map { Pair(it.current.point, it.current.direction.toBidirectional()) to it.score }
+                .forEach {
+                    val currentScore = visited[it.first] ?: Int.MAX_VALUE
+                    if (currentScore > it.second) {
+                        visited[it.first] = it.second
+                    }
+                }
+        }
+    }
+
+    private data class Path(val current: DirectedPoint, val visited: Set<Point>, val score: Int) {
+        constructor(point: Point) : this(DirectedPoint(point, Direction.RIGHT), setOf(point), 0)
+
+        fun move(): List<Path> =
+            getNeighbors()
+                .map { neighbor -> Path(neighbor.first, visited + neighbor.first.point, score + neighbor.second) }
+
+        private fun getNeighbors(): List<Pair<DirectedPoint, Int>> =
+            when (current.direction) {
                 Direction.UP -> listOf(
-                    MazePoint(point + Point.UP, Direction.UP, score + STRAIGHT_SCORE),
-                    MazePoint(point + Point.RIGHT, Direction.RIGHT, score + TURN_SCORE),
-                    MazePoint(point + Point.LEFT, Direction.LEFT, score + TURN_SCORE),
+                    Pair(DirectedPoint(current.point + Point.UP, Direction.UP), STRAIGHT_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.RIGHT), TURN_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.LEFT), TURN_SCORE),
                 )
 
                 Direction.RIGHT -> listOf(
-                    MazePoint(point + Point.RIGHT, Direction.RIGHT, score + STRAIGHT_SCORE),
-                    MazePoint(point + Point.DOWN, Direction.DOWN, score + TURN_SCORE),
-                    MazePoint(point + Point.UP, Direction.UP, score + TURN_SCORE),
+                    Pair(DirectedPoint(current.point + Point.RIGHT, Direction.RIGHT), STRAIGHT_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.DOWN), TURN_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.UP), TURN_SCORE),
                 )
 
                 Direction.DOWN -> listOf(
-                    MazePoint(point + Point.DOWN, Direction.DOWN, score + STRAIGHT_SCORE),
-                    MazePoint(point + Point.RIGHT, Direction.RIGHT, score + TURN_SCORE),
-                    MazePoint(point + Point.LEFT, Direction.LEFT, score + TURN_SCORE),
+                    Pair(DirectedPoint(current.point + Point.DOWN, Direction.DOWN), STRAIGHT_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.RIGHT), TURN_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.LEFT), TURN_SCORE),
                 )
 
                 Direction.LEFT -> listOf(
-                    MazePoint(point + Point.LEFT, Direction.LEFT, score + STRAIGHT_SCORE),
-                    MazePoint(point + Point.UP, Direction.UP, score + TURN_SCORE),
-                    MazePoint(point + Point.DOWN, Direction.DOWN, score + TURN_SCORE),
+                    Pair(DirectedPoint(current.point + Point.LEFT, Direction.LEFT), STRAIGHT_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.UP), TURN_SCORE),
+                    Pair(DirectedPoint(current.point, Direction.DOWN), TURN_SCORE),
                 )
             }
-        }
     }
 }
