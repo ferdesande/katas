8a014e3153cb7aaf0ca53ac398d7204a16743239
Fer de Sande <fernando.desande@bitingbit.com>
2025-01-01 17:14:50 +0000

    Solve Day 21

--- advent-of-code-2024-kotlin/src/main/kotlin/Day21.kt
+++ advent-of-code-2024-kotlin/src/main/kotlin/Day21.kt
@@ -7,8 +7,6 @@ class Day21 : AdventProblem {
 
     companion object {
         private const val INPUT = "/input21.txt"
-        private const val SAMPLE = "/input21-sample.txt"
-
     }
 
     private val pathFinder = BestPathFinder()
@@ -17,22 +15,31 @@ class Day21 : AdventProblem {
         // Valid result: 176650
         val values = FileUtils.getLines(INPUT).filter { it.isNotBlank() }
         val numericValue = values.map { it.dropLast(1) }.map { it.toInt() }
+
         return values
-            .map { "A$it" }
-            .map { path ->
-                path.zipWithNext { origin, destination -> pathFinder.getBestPath(origin, destination) }
-                    .joinToString("")
-            }
-            .zip(numericValue).map { (a, b) -> Pair(a.length, b) }.sumOf { it.first * it.second }.toString()
+            .map { numPadCode -> pathFinder.getDirectionPadCode("A$numPadCode") }
+            .map { directionalPadCode -> pathFinder.getBestPath(directionalPadCode, 2) }
+            .zip(numericValue)
+            .sumOf { it.first * it.second }.toString()
     }
 
     override fun secondPart(): String {
-        // Valid result:
-        val lines = FileUtils.getGrid(SAMPLE)
-        TODO()
+        // Valid result: 217698355426872
+        val values = FileUtils.getLines(INPUT).filter { it.isNotBlank() }
+        val numericValue = values.map { it.dropLast(1) }.map { it.toInt() }
+
+        return values
+            .map { numPadCode -> pathFinder.getDirectionPadCode("A$numPadCode") }
+            .map { directionalPadCode -> pathFinder.expandDirectionalPadCode(directionalPadCode, 1) }
+            .map { directionalPadCode -> pathFinder.expandDirectionalPadCode("A$directionalPadCode", 12) }
+            .map { directionalPadCode -> pathFinder.getBestPath("A$directionalPadCode", 12) }
+            .zip(numericValue)
+            .sumOf { it.first * it.second }.toString()
     }
 
-    private class BestPathFinder() {
+    private enum class Direction { UP, DOWN, LEFT, RIGHT }
+
+    private class BestPathFinder {
         companion object {
             private val numericKeyboard = mapOf(
                 '0' to Point(1, 3),
@@ -54,64 +61,23 @@ class Day21 : AdventProblem {
                 'v' to Point(1, 1),
                 '>' to Point(2, 1),
             )
-
-            private val numericKeyboardPaths = mapPath(numericKeyboard.values.toSet())
-            private val directionalKeyboardPaths = mapPath(directionalKeyboard.values.toSet())
-
-            private fun mapPath(validPoints: Set<Point>): Map<Pair<Point, Point>, Set<Set<Point>>> {
-                val result = mutableMapOf<Pair<Point, Point>, Set<Set<Point>>>()
-                for (origin in validPoints) {
-                    for (destination in validPoints) {
-                        if (origin != destination) {
-                            result[Pair(origin, destination)] = getPaths(origin, destination, validPoints)
-                        } else {
-                            result[Pair(origin, destination)] = setOf(setOf(origin))
-                        }
-                    }
-                }
-                return result
-            }
-
-            private fun getPaths(origin: Point, destination: Point, validPoints: Set<Point>): Set<Set<Point>> {
-                var current = setOf(setOf(origin))
-                while (current.isNotEmpty()) {
-                    current = current.flatMap { path ->
-                        path.last().getNeighbors()
-                            .filter { validPoints.contains(it) }
-                            .map { path + it }
-                            .toSet()
-                    }.toSet()
-
-                    val next = current.filter { it.last() == destination }
-                    if (next.isNotEmpty()) {
-                        return next.toSet()
-                    }
-                }
-                return emptySet()
-            }
         }
 
-        private val bestPath: Map<Pair<Point, Point>, String>
-
-        private fun getNextRobotPaths(
-            paths: List<String>,
-            pathByRoute: Map<Pair<Point, Point>, List<String>>
-        ): List<List<List<String>>> = paths.map { "A$it" }
-            .map { path ->
-                path.zipWithNext { a, b -> Pair(directionalKeyboard[a]!!, directionalKeyboard[b]!!) }
-                    .mapNotNull { pair -> pathByRoute[pair] }
-            }
+        private val numericKeyboardPathStrings: Map<Pair<Point, Point>, String>
+        private val bestPathsByDepth: MutableMap<Int, Map<Pair<Point, Point>, String>> = mutableMapOf()
 
         init {
+            val numericKeyboardPaths = mapPath(numericKeyboard.values.toSet())
+            val directionalKeyboardPaths = mapPath(directionalKeyboard.values.toSet())
 
-            val numericKeyboardPathStrings = numericKeyboardPaths.mapValues { entry ->
+            numericKeyboardPathStrings = numericKeyboardPaths.mapValues { entry ->
                 entry.value
                     .map { path ->
                         path.zipWithNext { a, b -> goTo(a, b) }
                             .mapNotNull { it }
                             .joinToString("")
                     }.map { "${it}A" }
-            }
+            }.mapValues { entry -> calculateBestPath(entry.value) }
 
             val directionalKeyboardPathStrings = directionalKeyboardPaths.mapValues { entry ->
                 entry.value
@@ -121,32 +87,47 @@ class Day21 : AdventProblem {
                             .joinToString("")
                     }
                     .map { "${it}A" }
-            }
+            }.mapValues { entry -> calculateBestPath(entry.value) }
 
-            val firstRobot = numericKeyboardPathStrings
-                .mapValues { entry -> getNextRobotPaths(entry.value, directionalKeyboardPathStrings) }
-
-            val secondRobot = firstRobot
-                .mapValues { entry ->
-                    entry.value.map { paths ->
-                        paths.map { path -> getNextRobotPaths(path, directionalKeyboardPathStrings) }
-                    }
-                }
-
-            bestPath = secondRobot.mapValues { entry ->
-                entry.value.map { numeric ->
-                    numeric.map { robot1 ->
-                        robot1.map { robot2 ->
-                            robot2.map { paths -> paths.minBy { path -> path.length } }.joinToString("")
-                        }
-                    }.map { robot1 -> robot1.minBy { path -> path.length } }.joinToString("")
-                }.minBy { path -> path.length }
-
-            }
+            bestPathsByDepth[1] = directionalKeyboardPathStrings
         }
 
-        fun getBestPath(origin: Char, destination: Char): String =
-            bestPath[Pair(numericKeyboard[origin], numericKeyboard[destination])]!!
+        fun getDirectionPadCode(numPadCode: String): String =
+            "A$numPadCode"
+                .zipWithNext { a, b -> Pair(numericKeyboard[a], numericKeyboard[b]) }
+                .mapNotNull { numericKeyboardPathStrings[it] }
+                .joinToString("")
+
+        fun getBestPath(directionalPadCode: String, depth: Int): Long {
+            expand(depth)
+
+            return directionalPadCode
+                .zipWithNext { a, b -> Pair(directionalKeyboard[a], directionalKeyboard[b]) }
+                .sumOf { (bestPathsByDepth[depth]?.get(it)?.length ?: 0).toLong() }
+        }
+
+        fun expandDirectionalPadCode(directionalPadCode: String, depth: Int): String {
+            expand(depth)
+
+            return directionalPadCode
+                .zipWithNext { a, b -> Pair(directionalKeyboard[a], directionalKeyboard[b]) }
+                .mapNotNull { bestPathsByDepth[depth]?.get(it) }
+                .joinToString("")
+        }
+
+        private fun expand(depth: Int) {
+            for (i in 1..depth) {
+                if (!bestPathsByDepth.contains(i)) {
+                    val path = bestPathsByDepth[i - 1]!!
+                    bestPathsByDepth[i] = path.mapValues { entry ->
+                        "A${entry.value}"
+                            .zipWithNext { a, b -> Pair(directionalKeyboard[a], directionalKeyboard[b]) }
+                            .mapNotNull { bestPathsByDepth[1]?.get(it) }
+                            .joinToString("")
+                    }
+                }
+            }
+        }
 
         private fun goTo(origin: Point, destination: Point): String? {
             return when (destination - origin) {
@@ -158,5 +139,78 @@ class Day21 : AdventProblem {
                 else -> null
             }
         }
+
+        private fun mapPath(validPoints: Set<Point>): Map<Pair<Point, Point>, Set<Set<Point>>> {
+            val result = mutableMapOf<Pair<Point, Point>, Set<Set<Point>>>()
+            for (origin in validPoints) {
+                for (destination in validPoints) {
+                    if (origin != destination) {
+                        result[Pair(origin, destination)] = getPaths(origin, destination, validPoints)
+                    } else {
+                        result[Pair(origin, destination)] = setOf(setOf(origin))
+                    }
+                }
+            }
+            return result
+        }
+
+        private fun getPaths(origin: Point, destination: Point, validPoints: Set<Point>): Set<Set<Point>> {
+            var current = setOf(setOf(origin))
+            while (current.isNotEmpty()) {
+                current = current.flatMap { path ->
+                    path.last().getNeighbors()
+                        .filter { validPoints.contains(it) }
+                        .map { path + it }
+                        .toSet()
+                }.toSet()
+
+                val next = current.filter { it.last() == destination }
+                if (next.isNotEmpty()) {
+                    return next.toSet()
+                }
+            }
+            return emptySet()
+        }
+
+        private fun calculateBestPath(paths: List<String>): String {
+            if (paths.size == 1)
+                return paths.first()
+
+            val options = paths.map { Pair(it, getDirections(it)) }
+            val minDirectionChanges = options.minOf { it.second.size }
+            val bestOptions = options
+                .filter { it.second.size == minDirectionChanges }
+
+            val upLeft = bestOptions.firstOrNull { it.second == listOf(Direction.LEFT, Direction.UP) }?.first
+            val downLeft = bestOptions.firstOrNull { it.second == listOf(Direction.LEFT, Direction.DOWN) }?.first
+            val upRight = bestOptions.firstOrNull { it.second == listOf(Direction.UP, Direction.RIGHT) }?.first
+            val downRight = bestOptions.firstOrNull { it.second == listOf(Direction.DOWN, Direction.RIGHT) }?.first
+
+            return if (bestOptions.size == 1) {
+                bestOptions.first().first
+            } else {
+                upLeft ?: downLeft ?: upRight ?: downRight ?: throw IllegalStateException("Algorithm failed")
+            }
+        }
+
+        private fun getDirections(path: String): List<Direction> =
+            path.mapNotNull { c ->
+                when (c) {
+                    '<' -> Direction.LEFT
+                    '^' -> Direction.UP
+                    '>' -> Direction.RIGHT
+                    'v' -> Direction.DOWN
+                    else -> null
+                }
+            }.zipWithNext { a, b -> if (a == b) null else a }
+                .filterNotNull() + path.mapNotNull { c ->
+                when (c) {
+                    '<' -> Direction.LEFT
+                    '^' -> Direction.UP
+                    '>' -> Direction.RIGHT
+                    'v' -> Direction.DOWN
+                    else -> null
+                }
+            }.last()
     }
 }
