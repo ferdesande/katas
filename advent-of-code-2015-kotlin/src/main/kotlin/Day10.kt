2ca944f0a35048fdea471cbac6532666cfc2eac3
Fer de Sande <fernando.desande@bitingbit.com>
2025-01-08 16:09:05 +0000

    Solve day 10 / 2015

---
+++ advent-of-code-2015-kotlin/src/main/kotlin/Day10.kt
@@ -0,0 +1,41 @@
+package org.fdesande
+
+import org.fdesande.common.FileUtils
+
+class Day10 : AdventProblem {
+
+    companion object {
+        private const val INPUT = "/input10.txt"
+    }
+
+    override fun firstPart(): String {
+        // Valid result: 252594
+        var value = FileUtils.getLines(INPUT).single() { it.isNotBlank() }
+
+        repeat(40) {value = process(value)}
+        return value.length.toString()
+    }
+
+    override fun secondPart(): String {
+        // Valid result: 3579328
+        var value = FileUtils.getLines(INPUT).single() { it.isNotBlank() }
+
+        repeat(50) {value = process(value)}
+        return value.length.toString()
+    }
+
+    private fun process(value: String): String {
+        var i = 0
+        val sb = StringBuilder()
+        while (i < value.length) {
+            val c = value[i]
+            var count = 1
+            while (i + count < value.length && value[i + count] == c) {
+                count++
+            }
+            sb.append("$count$c")
+            i += count
+        }
+        return sb.toString()
+    }
+}
